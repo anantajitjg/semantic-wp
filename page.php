f43c4a4560ef6ee97c8f0ca6d9c6edbdcb08ad66
anantajitjg <anantajitjg2010@gmail.com>
2017-10-18 21:30:59 +0530

    Comments Support

--- page.php
+++ page.php
@@ -4,13 +4,23 @@
     <?php if(have_posts()) : ?>
         <?php while(have_posts()) : the_post(); ?>
             <div class="column">
-                <div class="ui raised padded page segment">
-                    <h2 class="page-title"><?php the_title(); ?></h2>
-                    <div class="page-content"><?php the_content(); ?></div>
+                <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <div class="ui raised padded page segment">
+                        <h2 class="page-title"><?php the_title(); ?></h2>
+                        <div class="page-content"><?php the_content(); ?></div>
+                        <?php if(comments_open() || get_comments_number()) : ?>
+                            <?php comments_template(); ?>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </div>
-        <?php endwhile; else: ?>
-        <p><?php _e('Sorry! No page to display!', 'semanticwp'); ?></p>
+        <?php endwhile; ?>
+    <?php else: ?>
+            <div class="column">
+                <div class="ui raised segment">
+                    <p><?php _e('Sorry! No page to display!', 'semanticwp'); ?></p>
+                </div>
+            </div>
     <?php endif; ?>
     </div>
 </div>
