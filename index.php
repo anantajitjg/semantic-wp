409a0ef15d26429b3d4f884f01d1fc56a10b84d9
anantajitjg <anantajitjg2010@gmail.com>
2017-10-18 21:32:58 +0530

    Fix for Post not found

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <?php get_header(); ?>
 <div class="ui one column grid container">
     <div class="row">
-<?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <?php get_template_part('template-parts/content', 'none');  ?>
-        <?php endwhile; else : ?>
-        <p><?php _e("Sorry! Nothing to display.", "semanticwp"); ?></p>
-<?php endif; ?>
+    <?php if (have_posts()) :
+            while (have_posts()) : the_post();
+                get_template_part('template-parts/content', 'none');
+            endwhile; ?>
+    <?php else: ?>
+            <div class="column">
+                <div class="ui raised segment">
+                    <p><?php _e("Sorry! Nothing to display.", "semanticwp"); ?></p>
+                </div>
+            </div>
+    <?php endif; ?>
     </div>
 </div>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -1,11 +1,16 @@
 <?php get_header(); ?>
 <div class="ui grid container">
     <div class="row">
-    <?php if (have_posts()) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-            <?php get_template_part('template-parts/content', get_post_format());  ?>
-        <?php endwhile; else : ?>
-            <p><?php _e("Sorry! No posts to display.", "semanticwp"); ?></p>
+    <?php if (have_posts()) :
+            while (have_posts()) : the_post();
+                get_template_part('template-parts/content', get_post_format());
+            endwhile; ?>
+    <?php else: ?>
+        <div class="column">
+            <div class="ui raised segment">
+                <p><?php _e("Sorry! No posts to display.", "semanticwp"); ?></p>
+            </div>
+        </div>
     <?php endif; ?>
     </div>
 </div>
